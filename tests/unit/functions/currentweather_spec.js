f657b65580abe179e67efe1bd381b1ce5d44649c
kale-io <cos+a@iuvo.ai>
2019-01-23 20:24:09 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- tests/unit/functions/currentweather_spec.js
+++ tests/unit/functions/currentweather_spec.js
@@ -5,27 +5,27 @@ var expect = chai.expect;
 var vm = require("vm");
 
 
-describe("Functions module currentweather", function() {
+describe("Functions module currentweather", () => {
 
 
 	// Fake for use by currentweather.js
 	Module = {};
 	config = {};
 	Module.definitions = {};
-	Module.register = function (name, moduleDefinition) {
+	Module.register = (name, moduleDefinition) => {
 		Module.definitions[name] = moduleDefinition;
 	};
 
 
-	before(function(){
+	before(() => {
 		require("../../../modules/default/currentweather/currentweather.js");
 		Module.definitions.currentweather.config = {};
 	});
 
-	describe("roundValue", function() {
+	describe("roundValue", () => {
 
-		describe("this.config.roundTemp is true", function() {
-			before(function(){
+		describe("this.config.roundTemp is true", () => {
+			before(() => {
 				Module.definitions.currentweather.config.roundTemp = true;
 			});
 
@@ -33,25 +33,25 @@ describe("Functions module currentweather", function() {
 				// index 0 value
 				// index 1 expect
 				[1      ,  "1"],
-				[1.0    ,  "1"],
+				[1    ,  "1"],
 				[1.02   ,  "1"],
 				[10.12  , "10"],
-				[2.0    ,  "2"],
+				[2    ,  "2"],
 				["2.12" ,  "2"],
 				[10.1   , "10"]
 			]
 
 			values.forEach(value => {
-				it(`for ${value[0]} should be return ${value[1]}`, function() {
+				it(`for ${value[0]} should be return ${value[1]}`, () => {
 					expect(Module.definitions.currentweather.roundValue(value[0])).to.equal(value[1]);
 				});
 			});
 		});
 
 
-		describe("this.config.roundTemp is false", function() {
+		describe("this.config.roundTemp is false", () => {
 
-			before(function(){
+			before(() => {
 				Module.definitions.currentweather.config.roundTemp = false;
 			});
 
@@ -59,17 +59,17 @@ describe("Functions module currentweather", function() {
 				// index 0 value
 				// index 1 expect
 				[1      ,  "1.0"],
-				[1.0    ,  "1.0"],
+				[1    ,  "1.0"],
 				[1.02   ,  "1.0"],
 				[10.12  , "10.1"],
-				[2.0    ,  "2.0"],
+				[2    ,  "2.0"],
 				["2.12" ,  "2.1"],
 				[10.1   , "10.1"],
-				[10.10  , "10.1"]
+				[10.1  , "10.1"]
 			]
 
 			values.forEach(value => {
-				it(`for ${value[0]} should be return ${value[1]}`, function() {
+				it(`for ${value[0]} should be return ${value[1]}`, () => {
 					expect(Module.definitions.currentweather.roundValue(value[0])).to.equal(value[1]);
 				});
 			});
