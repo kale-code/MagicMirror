c01138bbe5f28ce71990057dd551266289cd9709
kale-io <cos+a@iuvo.ai>
2019-01-23 20:23:03 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- tests/e2e/modules/compliments_spec.js
+++ tests/e2e/modules/compliments_spec.js
@@ -14,82 +14,74 @@ describe("Compliments module", function() {
 
 	var app = null;
 
-	beforeEach(function() {
-		return helpers
+	beforeEach(() => helpers
 			.startApplication({
 				args: ["js/electron.js"]
 			})
-			.then(function(startedApp) {
+			.then(startedApp => {
 				app = startedApp;
-			});
-	});
+			}));
 
-	afterEach(function() {
-		return helpers.stopApplication(app);
-	});
+	afterEach(() => helpers.stopApplication(app));
 
-	describe("parts of days", function() {
-		before(function() {
+	describe("parts of days", () => {
+		before(() => {
 			// Set config sample for use in test
 			process.env.MM_CONFIG_FILE = "tests/configs/modules/compliments/compliments_parts_day.js";
 		});
 
-		it("if Morning compliments for that part of day", function() {
+		it("if Morning compliments for that part of day", () => {
 			var hour = new Date().getHours();
 			if (hour >= 3 && hour < 12) {
 				// if morning check
-				return app.client.waitUntilWindowLoaded().getText(".compliments").then(function(text) {
+				return app.client.waitUntilWindowLoaded().getText(".compliments").then(text => {
 					expect(text).to.be.oneOf(["Hi", "Good Morning", "Morning test"]);
 				});
 			}
 		});
 
-		it("if Afternoon show Compliments for that part of day", function() {
+		it("if Afternoon show Compliments for that part of day", () => {
 			var hour = new Date().getHours();
 			if (hour >= 12 && hour < 17) {
 				// if morning check
-				return app.client.waitUntilWindowLoaded().getText(".compliments").then(function(text) {
+				return app.client.waitUntilWindowLoaded().getText(".compliments").then(text => {
 					expect(text).to.be.oneOf(["Hello", "Good Afternoon", "Afternoon test"]);
 				});
 			}
 		});
 
-		it("if Evening show Compliments for that part of day", function() {
+		it("if Evening show Compliments for that part of day", () => {
 			var hour = new Date().getHours();
 			if (!(hour >= 3 && hour < 12) && !(hour >= 12 && hour < 17)) {
 				// if evening check
-				return app.client.waitUntilWindowLoaded().getText(".compliments").then(function(text) {
+				return app.client.waitUntilWindowLoaded().getText(".compliments").then(text => {
 					expect(text).to.be.oneOf(["Hello There", "Good Evening", "Evening test"]);
 				});
 			}
 		});
 	});
 
-	describe("Feature anytime in compliments module", function() {
-		describe("Set anytime and empty compliments for morning, evening and afternoon ", function() {
-			before(function() {
+	describe("Feature anytime in compliments module", () => {
+		describe("Set anytime and empty compliments for morning, evening and afternoon ", () => {
+			before(() => {
 				// Set config sample for use in test
 				process.env.MM_CONFIG_FILE = "tests/configs/modules/compliments/compliments_anytime.js";
 			});
 
-			it("Show anytime because if configure empty parts of day compliments and set anytime compliments", function() {
-				return app.client.waitUntilWindowLoaded().getText(".compliments").then(function(text) {
+			it("Show anytime because if configure empty parts of day compliments and set anytime compliments", () => app.client.waitUntilWindowLoaded().getText(".compliments").then(text => {
 					expect(text).to.be.oneOf(["Anytime here"]);
-				});
-			});
+				}));
 		});
 
-		describe("Only anytime present in configuration compliments", function() {
-			before(function() {
+		describe("Only anytime present in configuration compliments", () => {
+			before(() => {
 				// Set config sample for use in test
 				process.env.MM_CONFIG_FILE = "tests/configs/modules/compliments/compliments_only_anytime.js";
 			});
 
-			it("Show anytime compliments", function() {
-				return app.client.waitUntilWindowLoaded().getText(".compliments").then(function(text) {
+			it("Show anytime compliments", () => app.client.waitUntilWindowLoaded().getText(".compliments").then(text => {
 					expect(text).to.be.oneOf(["Anytime here"]);
-				});
-			});
+				}));
 		});
 	});
 });
