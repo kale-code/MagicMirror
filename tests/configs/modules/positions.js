e88f132fd36b6ea4124e0aa5e233fd8e75c62a10
kale-io <cos+a@iuvo.ai>
2019-01-23 20:22:08 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- tests/configs/modules/positions.js
+++ tests/configs/modules/positions.js
@@ -22,7 +22,7 @@ var config = {
 	},
 	modules:
 		// Using exotic content. This is why dont accept go to JSON configuration file
-		(function() {
+		(() => {
 			var positions = ["top_bar", "top_left", "top_center", "top_right", "upper_third",
 				"middle_center", "lower_third", "bottom_left", "bottom_center", "bottom_right",
 				"bottom_bar", "fullscreen_above", "fullscreen_below"];
