fd5d908161f5b062188da32507b2cd967cfa2d61
kale-io <cos+a@iuvo.ai>
2019-01-23 20:25:17 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- tests/unit/classes/deprecated_spec.js
+++ tests/unit/classes/deprecated_spec.js
@@ -2,12 +2,12 @@ const chai = require("chai");
 const expect = chai.expect;
 const deprecated = require("../../../js/deprecated");
 
-describe("Deprecated", function() {
-	it("should be an object", function() {
+describe("Deprecated", () => {
+	it("should be an object", () => {
 		expect(deprecated).to.be.an("object");
 	});
 
-	it("should contain configs array with deprecated options as strings", function() {
+	it("should contain configs array with deprecated options as strings", () => {
 		expect(deprecated.configs).to.be.an("array");
 		for (let option of deprecated.configs) {
 			expect(option).to.be.an("string");
